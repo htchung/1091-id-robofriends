fdaf191eca9bd795a357bdf9d3ac1d2dbdd8378f
htchung <htchung@gms.tku.edu.tw>
2020-10-14 15:51:43 +0800

    Updates

---
+++ precache-manifest.a8a3efb031b03e9b4a6571ce59220659.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5802b71517d6e387a116701049577feb",
+    "url": "/1091-id-robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/1091-id-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8c15c4d5a7164eb6d9ff",
+    "url": "/1091-id-robofriends/static/css/main.07d3f631.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/1091-id-robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1091-id-robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c15c4d5a7164eb6d9ff",
+    "url": "/1091-id-robofriends/static/js/main.c78c7023.chunk.js"
+  },
+  {
+    "revision": "a51abbcf008854cbf355",
+    "url": "/1091-id-robofriends/static/js/runtime-main.7f4955e0.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/1091-id-robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
